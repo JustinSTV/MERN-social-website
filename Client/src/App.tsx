819abf086c416fb0a05de5cd55b803215ba0b3b7
Justin S <steponaviciusjustas@gmail.com>
2024-12-30 12:11:45 +0200

    Routing, login, register and news feed pages

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
+import LoginPage from './components/pages/auth/LoginPage'
+import RegisterPage from './components/pages/auth/RegisterPage'
 
 const App = () => {
 
   return (
-    <>
-
-    </>
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      
+    </Routes>
   )
 }
 
