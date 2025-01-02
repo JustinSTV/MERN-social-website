0eeb8153ee79273b2193e52165222997d1ca25f6
Justin S <steponaviciusjustas@gmail.com>
2025-01-02 21:34:03 +0200

    formik/yup start, login page template

--- Client/src/components/pages/auth/LoginPage.tsx
+++ Client/src/components/pages/auth/LoginPage.tsx
@@ -1,7 +1,47 @@
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+
 const LoginPage = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validationSchema: Yup.object({
+
+    }),
+    onSubmit: async (values) => {
+      console.log(values)
+    }
+  })
+
   return (
     <section>
       <h1>Login</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input 
+            type="email" 
+            id="email" name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input 
+            type="password" 
+            id="password" name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+        </div>
+        <input type="submit" value="Login"/>
+      </form>
     </section>
   );
 }
