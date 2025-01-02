b2eac35ed0abcbfbb5f6cf85ba0a1919953a9545
Justin S <steponaviciusjustas@gmail.com>
2025-01-02 21:44:23 +0200

    register page template

--- Client/src/components/pages/auth/RegisterPage.tsx
+++ Client/src/components/pages/auth/RegisterPage.tsx
@@ -1,7 +1,69 @@
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+
 const RegisterPage = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+  
+    }),
+    onSubmit: async (values) => {
+      console.log(values)
+    }
+  })
+
   return (
     <section>
       <h1>Register</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input 
+            type="text" 
+            id="username" name="username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.username}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input 
+            type="email" 
+            id="email" name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input 
+            type="password" 
+            id="password" name="password"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password}
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password:</label>
+          <input 
+            type="password" 
+            id="confirmPassword" name="confirmPassword"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.confirmPassword}
+          />
+        </div>
+        <input type="submit" value="Register"/>
+      </form>
     </section>
   );
 }
