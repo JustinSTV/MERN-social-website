9340eb95041ea866f48030e42478e969962589d2
Justin S <steponaviciusjustas@gmail.com>
2025-02-08 13:54:10 +0200

    Multer package for image uploading and testing if images are saved in mongoDB

--- Server/src/routes/user/userRoute.js
+++ Server/src/routes/user/userRoute.js
@@ -1,13 +1,32 @@
-import { usersCollection, connectDB } from "../../config/dbConfig.js";
+import { usersCollection, imageCollection, connectDB } from "../../config/dbConfig.js";
 import { checkExistingUser } from "../../middleware/checkUser.js";
 import { authenticateToken } from "../../middleware/authMiddleware.js"
 
 import { Router } from "express";
 import bcrypt from "bcrypt";
 import { v4 as generateID } from "uuid";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+import multer from 'multer';
+import path from 'path';
 
 const router = Router();
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // filesize: 5Mb
+  fileFilter: (req, file, cb) => {
+    const allowedFileTypes = /jpeg|jpg|png/;
+    const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedFileTypes.test(file.mimetype);
+
+    if (extname && mimetype) {
+      return cb(null, true)
+    }
+    cb(new Error('Only images are allowed'));
+  }
+})
 
 router.post("/register", checkExistingUser, async (req, res) => {
   try {
@@ -130,6 +149,36 @@ router.put('/:userId', authenticateToken, async (req, res) => {
 
   } catch (error) {
     res.status(500).json({ message: "Failed to update profile" })
+  }
+})
+
+router.post('/test-upload', upload.single('image'), async (req, res) => {
+  try {
+    await connectDB()
+    if (!req.file) {
+      return res.status(400).json({ message: 'No file uploaded' })
+    }
+
+    const image = {
+      _id: generateID(),
+      filename: req.file.originalname,
+      contentType: req.file.mimetype,
+      uploadDate: new Date(),
+      data: req.file.buffer
+    };
+
+    await imageCollection.insertOne(image);
+
+    res.status(200).json({
+      message: 'File uploaded successfully',
+      fileId: image._id,
+      filename: image.filename
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: 'Upload failed',
+      error: error.message
+    })
   }
 })
 
